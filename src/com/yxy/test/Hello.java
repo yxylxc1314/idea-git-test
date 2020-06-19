498bc29e6443c719cf058c18d280bc1e4c567be6
yxylxc1314 <2720881698@qq.com>
2020-06-19 15:27:10 +0800

    modified the hello.java

--- src/com/yxy/test/Hello.java
+++ src/com/yxy/test/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("你好世界!");
+        System.out.println("git!");
     }
 }
